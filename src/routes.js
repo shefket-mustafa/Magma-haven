202613fadb26b243a8a73a127768bf2209e6bb08
shefket-mustafa <miracle_1@abv.bg>
2025-02-14 23:13:30 +0200

    register page
    Register page's endpoint added and page rendered(no functionality yet)

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import homeController from './controllers/home-controller.js';
+import authController from './controllers/auth-controller.js';
 
 const router = Router();
 
 router.use(homeController)
+router.use('/auth', authController);
 
 router.all('*', (req,res) => {
     res.render('404', {title: '404 Page'})
