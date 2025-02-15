599177360d5d92a3b661ebd1482d80efbf272f12
shefket-mustafa <miracle_1@abv.bg>
2025-02-15 13:54:57 +0200

    All volcanoes page rendered
    no functionality and dynamic yet

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -6,5 +6,8 @@ homeController.get('/', (req,res) => {
     res.render('home', {title: 'Home Page'});
 });
 
+homeController.get('/volcanoes', (req,res) => {
+    res.render('catalog', {title: 'All volcanoes'});
+})
 
 export default homeController;
